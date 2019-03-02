2f343caf95c790a9a1787ea1dd8bd241203a3876
Yuvraj Gupta <yuvraj@Yuvrajs-MacBook-Air.local>
2019-03-01 21:21:08 -0500

    UpdateSetupFile

--- Setup-Environment.sh
+++ Setup-Environment.sh
@@ -12,25 +12,25 @@ STORAGE_SKU="Standard_LRS"
 ACR_REGISTRY_NAME="dockerrepodemo"
 ACR_SKU="Basic"
 
-#Create Resource Group
+# Create Resource Group
 az group create --name=${AZURE_RESOURCE_GROUP} --location=${AZURE_LOCATION}
 
-#Create AKS Cluster
+# Create AKS Cluster
 az aks create --resource-group ${AZURE_RESOURCE_GROUP} --name ${AKS_CLUSTER_NAME} --generate-ssh-keys --node-vm-size ${AKS_NODE_SIZE} --node-count ${AKS_NODE_COUNT} --kubernetes-version ${AKS_VERSION}
 
-#Connect to AKS Cluster
+# Connect to AKS Cluster
 az aks get-credentials --resource-group ${AZURE_RESOURCE_GROUP} --name ${AKS_CLUSTER_NAME}
 
-#Create Storage Account
+# Create Storage Account
 az storage account create --name ${AZURE_STORAGE_ACCOUNT} --location ${AZURE_LOCATION} --resource-group ${AZURE_RESOURCE_GROUP} --kind Storage --sku=${STORAGE_SKU}
 
 # Retrieve the Azure Storage Account Key
 AZURE_STORAGE_KEY=`az storage account keys list --account-name ${AZURE_STORAGE_ACCOUNT} --resource-group ${AZURE_RESOURCE_GROUP} --output tsv | grep "key1" | cut -d $'\t' -f3`
 
-#Create Container within a Storage Account
+# Create Container within a Storage Account
 az storage container create --name ${AZURE_BLOB_CONTAINER_NAME} --account-key ${AZURE_STORAGE_KEY} --account-name ${AZURE_STORAGE_ACCOUNT}
 
-# Install PachCTL
+# Install PachCTL (Currently Command is for installation on Linux)
 curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v1.7.1/pachctl_1.7.1_amd64.deb && sudo dpkg -i /tmp/pachctl.deb
 
 # Start Pachyderm
