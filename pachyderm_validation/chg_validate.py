dbe58860208898355baa5628a1c932ca5e6c5130
Karma Patel <karma@purdue.edu>
2019-03-04 19:21:15 -0500

    Update chg_validate.py

--- pachyderm_validation/chg_validate.py
+++ pachyderm_validation/chg_validate.py
@@ -2,11 +2,27 @@ import os, uuid, sys
 from azure.storage.blob import BlockBlobService, PublicAccess
 from dateutil.parser import parse
 import datetime
+import pyodbc
 
 # Mentioning the storage account name and key
 act_name = "crowedemostorage"
 act_key = "WWxjJW+GkKIkklkXEuR6nDalogfrriKUG3Ra03Z3/xiwg5EA3lOVutjqqJbxCBCxd9C8HrtAuV6OC0Nzwz1rbQ=="
 block_blob_service = BlockBlobService(account_name=act_name, account_key=act_key)
+
+# Database connection
+
+server = 'sql-demo-server.database.windows.net'
+database = 'sql-db'
+username = 'crowe'
+password = 'Practicum_2019'
+driver= '{ODBC Driver 17 for SQL Server}'
+connection = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = connection.cursor()
+cursor.execute("SELECT @@version;")
+row = cursor.fetchone()
+while row:
+    print (row[0]) 
+    row = cursor.fetchone()
 
 # Creating function to move file from one blob container to another
 
@@ -30,6 +46,8 @@ validate_failure_container_name="validate-failure"
 block_blob_service.create_container(processed_container_name)
 block_blob_service.create_container(validate_success_container_name)
 block_blob_service.create_container(validate_failure_container_name)
+fields_string = "(ChargeDataID, AccountID, FacilityID, PostingDate, HCPCSCode, ModifierComboCode, ProvidedRVUCode, PlaceOfServiceID, CarrierNumberCode, RevenueCodeID, PhysicianNPIComboCode, DaysUnits, ChargeAmount, TotalCharges)"
+value_placeholder_string = "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 # Iterate through the blobs in the container
 
@@ -170,16 +188,94 @@ def validate_file(file_name):
 		validate_status = False
 	return validate_status
 
+# Function to load data into list
+
+def load_data(file_name):
+	open_file = open(path+file_name,'r').readlines()
+	data_master_array = []
+	for line in open_file[1:]:
+    	current_line = line.strip()
+    	data_master_array.append(current_line.split("|"))
+	return(data_master_array)
+
+# Function to delete all records (Temporary soln)
+
+def delall():
+	tem_sql = ("DELETE from dbo.factChargeData")
+	cursor.execute(tem_sql)
+
+# Function to insert a record
+
+def insert_record(current_data, counter):
+    isvalid = True
+    values = []
+    values.append(counter + 1)
+    values.append(counter + 1)
+    values.append(counter + 1)
+    if(current_data[3] is not None and current_data[3] != ""):
+        try:
+            value_date = datetime.strptime(current_data[3] + " 00:00:00",'%m/%d/%Y %H:%M:%S')
+            values.append(value_date)
+        except ValueError as ve:
+            isvalid = False
+            return(isvalid)
+    else:
+        isvalid = False
+        return(isvalid)
+    values.append(int(current_data[4]))
+    values.append(str(current_data[5]) + "," + str(current_data[6]) + "," + str(current_data[7]) + "," + str(current_data[8]))
+    values.append(str(current_data[9]))
+    values.append(str(current_data[10]))
+    values.append(str(current_data[11]))
+    values.append(str(current_data[13]))
+    values.append(str(current_data[14]) + "," + str(current_data[15]) + "," + str(current_data[16]) + "," + str(current_data[17]))
+    values.append(float(current_data[18]))
+    values.append(str(current_data[19]))
+    values.append(str(current_data[20]))
+    sql_command = "INSERT INTO dbo.factChargeData " + fields_string + " VALUES " +  value_placeholder_string
+    print(sql_command)
+    print(values)
+    cursor.execute(sql_command,values)
+    return(isvalid)
+
+# Function to insert data from file
+
+total_count = len(data_master_array)
+
+def insert_file(data_master_array):
+    isvalid = True
+    for counter in range(len(data_master_array)):
+        current_data = data_master_array[counter]
+        if(isvalid):
+            isvalid = insert_record(current_data, counter)
+            if(not isvalid):
+                return(isvalid)
+    return(isvalid)
+
 path = '/pfs/query/'
+valid_data = True
 for filename in os.listdir(path):
 	if validate_file(filename):
-		print(filename,":Success")
-		print("Copying file to Validate Success Container")
-		print(validate_success_container_name, filename, filename)
-		moveFileToAnotherContainer(container_name,validate_success_container_name,filename)
-
+		data_master_array = load_data(filename)
+		delall()
+		valid_data = insert_file(data_master_array)
+		if(valid_data):
+			connection.commit()
+			print(filename,":Success")
+			print("{} Records Inserted".format((total_count)))
+			print("Copying file to Validate Success Container")
+			print(validate_success_container_name, filename, filename)
+			moveFileToAnotherContainer(container_name,validate_success_container_name,filename)
+		else:
+			print(filename,":Fail")
+			print("Invalid data found, record insert failed")
+			print("Copying file to Validate Failure Container")
+			print(validate_failure_container_name, filename, filename)
+			moveFileToAnotherContainer(container_name,validate_failure_container_name,filename)
 	else:
 		print(filename,":Fail")
 		print("Copying file to Validate Failure Container")
 		print(validate_failure_container_name, filename, filename)
 		moveFileToAnotherContainer(container_name,validate_failure_container_name,filename)
+connection.close()
+
